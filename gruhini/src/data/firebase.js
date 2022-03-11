6cacfbf83035d08bd1ed4cc532ba61026e04a371
Krishna3267 <iit2020134@iiita.ac.in>
2022-03-12 00:56:04 +0530

    Added db

---
+++ gruhini/src/data/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAaK3nWF2Lvyf8JwsF3WkQjs5HcaAuRNvY",
+  authDomain: "gruhini-efac0.firebaseapp.com",
+  projectId: "gruhini-efac0",
+  storageBucket: "gruhini-efac0.appspot.com",
+  messagingSenderId: "692630689537",
+  appId: "1:692630689537:web:2fbb3a02d5c36c17abed00"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
