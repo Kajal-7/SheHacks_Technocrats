613819eb5be2f024a73e2990571e4893f5a94d51
Kajal <kajalkaushal700@gmail.com>
2022-03-11 23:12:45 +0530

    Added images

--- gruhini/src/App.js
+++ gruhini/src/App.js
@@ -1,9 +1,71 @@
-import "./App.css";
+import React, { useEffect, useState } from "react";
+import "./style/App.css";
+import GetStarted from "./GetStarted";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./components/Authentication/Login";
+import Register from "./components/Authentication/Register";
+import Empower from "./Empower";
+import { Authentication } from "./data/auth";
+import PrivateRoute from "./components/PrivateRoute";
+import ForgotPassword from "./components/Authentication/ForgotPassword";
+import Loader from "./components/Loader";
+import PostJob from "./PostJob";
+import FindJob from "./FindJob";
+import SellProduct from "./SellProduct";
+import BuyProduct from "./BuyProduct";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">Hello Shehacks</header>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Authentication
+          child={
+            <Router>
+              <Routes>
+                <Route exact path={"/"} element={<GetStarted />} />
+                <Route path={"/login"} element={<Login />} />
+                <Route path={"/register"} element={<Register />} />
+                <Route
+                  path={"/empower"}
+                  element={<PrivateRoute child={<Empower />}> </PrivateRoute>}
+                />
+                <Route
+                  path={"/findJob"}
+                  element={<PrivateRoute child={<FindJob />}> </PrivateRoute>}
+                />
+                <Route
+                  path={"/postJob"}
+                  element={<PrivateRoute child={<PostJob />}> </PrivateRoute>}
+                />
+                <Route
+                  path={"/sellProduct"}
+                  element={
+                    <PrivateRoute child={<SellProduct />}> </PrivateRoute>
+                  }
+                />
+                <Route
+                  path={"/buyProduct"}
+                  element={
+                    <PrivateRoute child={<BuyProduct />}> </PrivateRoute>
+                  }
+                />
+                <Route path={"/forgotpassword"} element={<ForgotPassword />} />
+              </Routes>
+            </Router>
+          }
+        />
+      )}
     </div>
   );
 }
