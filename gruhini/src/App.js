3191cd62113152d9d5121ab92f90da00fa06cc3d
Laalasa <iit2020178@iiita.ac.in>
2022-03-11 22:43:40 +0530

    Added React Template

---
+++ gruhini/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Hello Shehacks</header>
+    </div>
+  );
+}
+
+export default App;
