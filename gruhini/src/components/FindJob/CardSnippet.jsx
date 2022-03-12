f6bf3ace919d182d3b7575841d184c3d7e08ee12
Laalasa <iit2020178@iiita.ac.in>
2022-03-13 00:49:40 +0530

    Resolved Conflicts

--- gruhini/src/components/FindJob/CardSnippet.jsx
+++ gruhini/src/components/FindJob/CardSnippet.jsx
@@ -17,16 +17,13 @@ import WorkIcon from "@mui/icons-material/Work";
 import data from "./data";
 import Filter from "./Filter";
 import Popup from "./Popup";
-import { doc, getDoc } from "firebase/firestore";
-import { db } from "../../data/firebase";
-import { Avatar } from "@mui/material";
 
 export default function CardSnippet() {
   const [arr, setarray] = useState(data);
   const [locFilter, setlocFilter] = useState("");
   const [catgfilter, setcatgfilter] = useState("");
-  const [isParttimeselected, setPartTime] = useState(true);
-  const [isFulltimeselected, setFullTime] = useState(true);
+  const [isParttimeselected, setPartTime] = useState(false);
+  const [isFulltimeselected, setFullTime] = useState(false);
 
   const handleFilters = () => {
     let data1 = [];
@@ -61,8 +58,22 @@ export default function CardSnippet() {
           data1.push(data[i]);
       }
     }
+    let data2 = [];
+    let len2 = data1.length;
 
-    setarray(data1);
+    if (!isFulltimeselected && isParttimeselected) {
+      for (let i = 0; i < len2; i++) {
+        if (data1[i].type == "Part Time") data2.push(data1[i]);
+      }
+    } else if (!isParttimeselected && isFulltimeselected) {
+      for (let i = 0; i < len2; i++) {
+        if (data1[i].type == "Full Time") data2.push(data1[i]);
+      }
+    } else {
+      data2 = data1;
+    }
+
+    setarray(data2);
   };
   const [searchText, setSearchText] = useState("");
   const handleSearch = (event) => {
@@ -84,122 +95,110 @@ export default function CardSnippet() {
         locFilter={locFilter}
         catgfilter={catgfilter}
         searchText={searchText}
+        partTime={isParttimeselected}
+        fullTime={isFulltimeselected}
         onLocSet={setlocFilter}
         onCatgSet={setcatgfilter}
         onSave={handleFilters}
         onSearch={handleSearch}
+        onPartTimeCheck={setPartTime}
+        onFullTimeCheck={setFullTime}
       />
       <Grid container spacing={5}>
         {arr.map((item) => {
           return (
-            <div>
-              <Grid item xs={12} sm={12} md={6} lg={4}>
-                {getDoc(doc(db, "users", item.userId))
-                  .then((snap) => {
-                    
-                    console.log(snap.data())
-                  })
-                  .catch((e) => {
-                    console.log(e);
-                  })}
-                <Box sx={{ minWidth: 275 }}>
-                  <Card variant="outlined">
-                    <CardContent>
-                      <Typography
-                        className="name"
-                        gutterBottom
-                        variant="h5"
-                        component="div"
-                      >
-                        <div className="row">
-                          <div className="col-9">
-                            {item.jobName} -{" "}
-                            <span className="jobtype">{item.type}</span>
-                          </div>
-                          <div className="col-3">
-                            <Popup
-                              jobTitle={item.jobName}
-                              type={item.type}
-                              name={item.title}
-                              desc={item.Description}
-                              duration={item.Duration}
-                              stipend={item.Salary}
-                              deadline={item.Deadline}
-                              phone={item.PhoneNo}
-                              email={item.Email}
-                              fullName={item.FullName}
-                              location={item.location}
-                            />
-                          </div>
+            <Grid item xs={12} sm={12} md={6} lg={4}>
+              <Box sx={{ minWidth: 275 }}>
+                <Card className="job-card txt" variant="outlined">
+                  <CardContent>
+                    <Typography
+                      className="name"
+                      gutterBottom
+                      variant="h5"
+                      component="div"
+                    >
+                      <div className="row">
+                        <div className="col-9">
+                          {item.jobName} -{" "}
+                          <span className="jobtype">{item.type}</span>
                         </div>
-                      </Typography>
-                      <Typography>
-                        <div className="d-flex title">
-                          {item.title}
-                          &nbsp;&nbsp;&nbsp;
-                          <Star />
+                        <div className="col-3">
+                          <Popup
+                            jobTitle={item.jobName}
+                            type={item.type}
+                            name={item.title}
+                            desc={item.Description}
+                            duration={item.Duration}
+                            stipend={item.Salary}
+                            deadline={item.Deadline}
+                            phone={item.PhoneNo}
+                            email={item.Email}
+                            fullName={item.FullName}
+                            location={item.location}
+                          />
                         </div>
-                      </Typography>
+                      </div>
+                    </Typography>
+                    <Typography>
+                      <div className="d-flex title">
+                        {item.title}
+                        &nbsp;&nbsp;&nbsp;
+                        <Star />
+                      </div>
+                    </Typography>
 
-                      <div>
-                        <div
-                          className="info-small"
-                          style={{ display: "inline-block" }}
-                        >
-                          <div
-                            className="tag"
-                            style={{ display: "inline-flex" }}
-                          >
-                            <LocationOnOutlinedIcon /> {item.location}
-                          </div>
-                        </div>
-                        <div
-                          className="info-small"
-                          style={{ display: "inline-block" }}
-                        >
-                          <div
-                            className="tag"
-                            style={{ display: "inline-flex" }}
-                          >
-                            <PhoneInTalkOutlinedIcon />
-                            {item.PhoneNo}
-                          </div>
-                        </div>
-                      </div>
+                    <div>
                       <div
-                        className="text-center info mt-5"
+                        className="info-small"
                         style={{ display: "inline-block" }}
                       >
                         <div className="tag" style={{ display: "inline-flex" }}>
-                          <EventAvailableOutlinedIcon /> Duration
+                          <LocationOnOutlinedIcon /> {item.location}
                         </div>
-                        <div className="value">{item.Duration}</div>
                       </div>
                       <div
-                        className="info text-center"
+                        className="info-small"
                         style={{ display: "inline-block" }}
                       >
                         <div className="tag" style={{ display: "inline-flex" }}>
-                          <PaidOutlinedIcon />
-                          Stipend
+                          <PhoneInTalkOutlinedIcon />
+                          {item.PhoneNo}
                         </div>
-                        <div className="value">{item.Salary}</div>
                       </div>
-                      <div
-                        className="info text-center"
-                        style={{ display: "inline-block" }}
-                      >
-                        <div className="tag" style={{ display: "inline-flex" }}>
-                          <AccessTimeRoundedIcon />
-                          Apply by
-                        </div>
-                        <div className="value">{item.Deadline}</div>
+                    </div>
+                    <div
+                      className="text-center info mt-5"
+                      style={{ display: "inline-block" }}
+                    >
+                      <div className="tag" style={{ display: "inline-flex" }}>
+                        <EventAvailableOutlinedIcon /> Duration
                       </div>
-                    </CardContent>
-                  </Card>
-                </Box>
-              </Grid>
-            </div>
+                      <div className="value">{item.Duration}</div>
+                    </div>
+                    <div
+                      className="info text-center"
+                      style={{ display: "inline-block" }}
+                    >
+                      <div className="tag" style={{ display: "inline-flex" }}>
+                        <PaidOutlinedIcon />
+                        Stipend
+                      </div>
+                      <div className="value">{item.Salary}</div>
+                    </div>
+                    <div
+                      className="info text-center"
+                      style={{ display: "inline-block" }}
+                    >
+                      <div className="tag" style={{ display: "inline-flex" }}>
+                        <AccessTimeRoundedIcon />
+                        Apply by
+                      </div>
+                      <div className="value">{item.Deadline}</div>
+                    </div>
+                  </CardContent>
+                </Card>
+              </Box>
+            </Grid>
           );
         })}
       </Grid>
