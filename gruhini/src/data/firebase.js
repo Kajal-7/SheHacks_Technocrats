91fb6d4a84c0fbfae77c839ee3cd75a5a4b5cb93
Krishna3267 <iit2020134@iiita.ac.in>
2022-03-12 20:38:26 +0530

    Data

--- gruhini/src/data/firebase.js
+++ gruhini/src/data/firebase.js
@@ -2,10 +2,8 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getStorage, uploadBytes } from "firebase/storage";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAaK3nWF2Lvyf8JwsF3WkQjs5HcaAuRNvY",
   authDomain: "gruhini-efac0.firebaseapp.com",
@@ -19,3 +17,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage();
